c808a2809bcaf44ef5a43b1aff05b3fde917b50b
nonfunjible <108364599+nonfunjible@users.noreply.github.com>
2023-01-01 16:43:09 -0500

    Create py5-3rd-example-from-py5bot.py

---
+++ py5-3rd-example-from-py5bot.py
@@ -0,0 +1,29 @@
+"""
+Adapted from 3rd example at
+https://py5coding.org/tutorials/introduction_to_py5bot.html
+to produce this:
+https://py5coding.org/_images/introduction_to_py5bot_7_1.png 
+and to include a print() statement.
+
+Code modified to Module Mode.
+"""
+
+import py5
+
+def setup():
+    py5.size(200, 200)
+    print('draw a red rectangle')
+    py5.fill(255, 0, 0)
+    py5.rect(50, 50, 100, 100)
+
+py5.run_sketch()
+
+import py5
+
+def setup():
+    py5.size(200, 200)
+    print('draw a red rectangle')
+    py5.fill(255, 0, 0)
+    py5.rect(50, 50, 100, 100)
+
+py5.run_sketch()
